a3f6524b8045f1bcdba12fa25634aa8dcde4aa70
HMoss <htmoss7@gmail.com>
2021-11-19 21:35:43 -0800

    added iso codes to csv files

---
+++ convert_table.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+a = pd.read_csv("2019.csv")
+b = pd.read_csv("country-codes.csv")
+# b = b.dropna(axis=1)
+merged = a.merge(b, left_on='Country or region',
+                 right_on='English short name lower case')
+merged = merged.drop(['English short name lower case', 'Alpha-2 code',
+                      'Numeric code', 'ISO 3166-2'], axis=1)
+merged.to_csv("output.csv", index=False)
